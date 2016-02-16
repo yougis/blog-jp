9cf25ac4bc74127dd99f5173277d817b9a2eaf29
Hugo Roussaffa <hugoroussaffa@gmail.com>
2016-02-16 18:27:42 +0100

    flicker api conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,7 +17,10 @@ DATE_FORMATS = {
     'jp': '%Y-%m-%d(%a)',
     'fr': '%d %m %Y'}
 
-PLUGIN_PATHS = ['../pelican-plugins/']
+PLUGIN_PATHS = 
+['../pelican-plugins/',
+'pelican-flickr',
+]
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
            'liquid_tags.include_code',
