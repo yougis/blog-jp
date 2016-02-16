d9814f19c1b55e2e1e5e7113a267bfcc65cba047
Hugo Roussaffa <hugoroussaffa@gmail.com>
2016-02-16 14:41:25 +0100

    initial commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Yo Gis Production'
-SITENAME = u'Yo Gis'
-SITEURL = 'http://yogis.alwaysdata.net'
+AUTHOR = u'Jean Pierre Roussaffa'
+SITENAME = u'JP'
+SITEURL = 'http://jproussaffa.alwaysdata.net'
 
 PATH = 'content'
 
@@ -37,14 +37,18 @@ TAG_CLOUD_MAX_ITEMS = 100
 
 COVER_IMG_URL = '/../../images/font2.jpg'
 PROFILE_IMG_URL = '/../../images/yogis_stamp_black_little.jpg'
-TAGLINE = 'Geomatic, Image & Sound'
+TAGLINE = 'tagline'
 #FAVICON_URL - Set the favicon image
-DISQUS_SITENAME = "yogis"
+DISQUS_SITENAME = "jproussaffa"
 DISQUS_ON_PAGES = True
 #GOOGLE_ANALYTICS - Set the Google Analytics code (eg. "UA-000000-00")
 #PIWIK_URL and PIWIK_SITE_ID - Set the URL and site-id for Piwik tracking. (Without 'http://')
-MENUITEMS = (('Geomatic', 'pages/project-manager-gis-administrator-and-spatial-analyst.html'),
-('Image Sound Prod', 'pages/we-are-prod.html'),('Claire Cousergue', 'pages/claire-cousergue-camerawoman-location-manager.html'),('Gallery', 'pages/my-albums.html'),('Archives','archives.html'))
+MENUITEMS = (
+  #('Geomatic', 'pages/project-manager-gis-administrator-and-spatial-analyst.html'),
+  #('Image Sound Prod', 'pages/we-are-prod.html'),('Claire Cousergue', 
+  #  'pages/claire-cousergue-camerawoman-location-manager.html'),
+  #('Gallery', 'pages/my-albums.html'),('Archives','archives.html')
+  )
 
 STATIC_PATHS = ['images']
 
@@ -72,20 +76,20 @@ SITEMAP = {
 
 PDF_GENERATOR = False
 
-FLICKR_API_KEY = '51b43627304c37e85055ba4235ffe9dd'
-FLICKR_API_SECRET = '5f49bbdfc0b7e732'
-FLICKR_USER = '131609124@N08'
+#FLICKR_API_KEY = '51b43627304c37e85055ba4235ffe9dd'
+#FLICKR_API_SECRET = '5f49bbdfc0b7e732'
+#FLICKR_USER = '131609124@N08'
 
 
 # Blogroll
 #LINKS = ((,),(,))
 
 # Social widget
-SOCIAL = (('soundcloud', 'https://soundcloud.com/yogis-record'),
+SOCIAL = (#('soundcloud', 'https://soundcloud.com/yogis-record'),
           ('flickr','https://www.flickr.com/photos/131609124@N08'),
-          ('youtube', 'https://www.youtube.com/channel/UCK6L4K87OB9cQMdMZRnz5hg'),
-		  ('map-marker','https://www.openstreetmap.org/user/goym@p/history'),
-		  ('github-square','https://github.com/yougis'),
+          #('youtube', 'https://www.youtube.com/channel/UCK6L4K87OB9cQMdMZRnz5hg'),
+		  #('map-marker','https://www.openstreetmap.org/user/goym@p/history'),
+		  #('github-square','https://github.com/yougis'),
       ('rss-square',FEED_ALL_ATOM))
 
 DEFAULT_PAGINATION = 10
